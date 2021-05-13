3c55fc9593ca943f4a8066d5804e2c46019ea83e
Vigdorchik <michael.vigdorchik@intel.com>
2021-05-13 13:40:43 +0300

    new controller

--- main/controller.py
+++ main/controller.py
@@ -1,5 +1,6 @@
 import serial
 import time
+from sys import stdout
 
 
 # uart propreties
@@ -12,9 +13,11 @@ s = serial.Serial(port=PORT, baudrate=BAUD, timeout=TIMEOUT)
 
 
 def writer(data: str):
-    print(data)
-    print(type(data))
-    s.write(bytes(data, 'utf-8'))
+    if data == "":
+        # user entered not data.
+        # happens when just pressing "ENTER" key, to pass input function
+        data = " "  # instead insert a SPACE character (0x20)
+    s.write(bytes(data, "utf-8"))
     s.flush()
 
 
@@ -23,14 +26,19 @@ def reader():
 
     """
     recv = "0"
-    while recv:
+
+    while recv != "":
         try:
-            recv = s.readline()
-            print(recv)
+            recv = s.readline()  # read a '\n' terminated line or timeout
+            recv = recv.decode("cp1252")  # decodes utf-8 and more
+            stdout.write(recv)
+            stdout.flush()
+
             if ">" in recv:  # user input is required
                 return True
         
-        except TimeoutError:
+        except serial.SerialException:
+            print("Reader TimeoutError")
             break
     return None
     
@@ -40,24 +48,34 @@ def init_arduino():
     """
     read arduino "start message" and send a start signal
     """
-    print(s.readlines(2))
-    if input(">") == "s":
-        write("s")
+    # wait for arduino to send start message
+    while reader() is None:
+        pass
+    # wait for user to send a start char to Arduino
+    while input() != "s":
+        pass
+    writer("s")
 
 
 def main():
     s.reset_input_buffer()
     s.reset_output_buffer()
+    s.flushInput()
+    s.flushOutput()
+
+    # Starting Arduino
     init_arduino()
     
     # Entering Arduino's main menu
     while True:
-        if reader() is not None:
-            input()
+        r = reader()
+        if r is True:
+            command = input()
+            writer(command)
 
-    
-    
-
+        if command == 'z':
+            reader()
+            break    
     s.close()
 
 if __name__ == "__main__":
